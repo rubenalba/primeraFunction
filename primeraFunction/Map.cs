1b802a8471d6696c84e21773bb5e90eb7ceeaabc
Ruben Alba Revilla <ruben.alba@viseo.com>
2020-02-14 13:24:30 +0100

    tableentity

---
+++ primeraFunction/Map.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace primeraFunction
+{
+    public class Map
+    {
+    }
+}
---
+++ primeraFunction/UserTableEntity.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace primeraFunction
+{
+     public class UserTableEntity : TableEntity
+    {
+        public string Nombre { get; set; }
+        public string Rol { get; set; }
+    }
+}

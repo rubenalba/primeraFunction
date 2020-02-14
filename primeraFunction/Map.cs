0a967efeb922bba29381aea9dc2182e96c2a011f
Ruben Alba Revilla <ruben.alba@viseo.com>
2020-02-14 13:33:42 +0100

    mapeo

--- primeraFunction/Map.cs
+++ primeraFunction/Map.cs
@@ -4,7 +4,31 @@ using System.Text;
 
 namespace primeraFunction
 {
-    public class Map
+    public static class Map
     {
+        public static UserTableEntity ToTableEntity(this User user)
+        {
+            return new UserTableEntity()
+            {
+                Nombre = user.Nombre,
+                Rol = user.Rol,
+                PartitionKey = "Spain",
+                RowKey = user.RowKey,
+                Timestamp = user.Timestamp
+            };
+        }
+
+        public static User ToUser(this UserTableEntity user)
+        {
+            return new User()
+            {
+                Nombre = user.Nombre,
+                PartitionKey = "Spain",
+                Rol = user.Rol,
+                RowKey = user.RowKey,
+                Timestamp = user.Timestamp
+            };
+        }
+            
     }
 }
